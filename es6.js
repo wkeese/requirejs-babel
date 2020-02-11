a03ae30e64a4658c51080dd987bc5b7d9ea7fee7
Bill Keese <bill@dojotoolkit.org>
2020-02-11 21:50:35 +0900

    remove builder code

--- es6.js
+++ es6.js
@@ -1,61 +1,24 @@
 define([
-//>>excludeStart('excludeBabel', pragmas.excludeBabel)
-	'babel',
-	'module'
-//>>excludeEnd('excludeBabel')
+	"babel"
 ], function (
-//>>excludeStart('excludeBabel', pragmas.excludeBabel)
-babel,
-_module
-//>>excludeEnd('excludeBabel')
+	babel
 ) {
-//>>excludeStart('excludeBabel', pragmas.excludeBabel)
-	var fetchText, _buildMap = {};
-
-	if (typeof window !== 'undefined' && window.navigator && window.document) {
-		fetchText = function (url, callback) {
-			var xhr = new XMLHttpRequest();
-			xhr.open('GET', url, true);
-			xhr.onreadystatechange = function (evt) {
-				//Do not explicitly handle errors, those should be
-				//visible via console output in the browser.
-				if (xhr.readyState === 4) {
-					callback(xhr.responseText);
-				}
-			};
-			xhr.send(null);
+	var fetchText = function (url, callback) {
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", url, true);
+		xhr.onreadystatechange = function () {
+			// Do not explicitly handle errors, those should be
+			// visible via console output in the browser.
+			if (xhr.readyState === 4) {
+				callback(xhr.responseText);
+			}
 		};
-	} else if (typeof process !== 'undefined' &&
-		process.versions &&
-		!!process.versions.node) {
-		//Using special require.nodeRequire, something added by r.js.
-		var fs = require.nodeRequire('fs');
-		fetchText = function (path, callback) {
-			callback(fs.readFileSync(path, 'utf8'));
-		};
-	}
+		xhr.send(null);
+	};
 
-	function resolvePath(nodePath, state) {
-		if (!state.types.isStringLiteral(nodePath)) {
-			return;
-		}
-
-		const sourcePath = nodePath.node.value;
-		if (sourcePath.indexOf('!') < 0) {
-			// If sourcePath is relative (ex: "./foo"), it's relative to currentFile.
-			const currentFile = state.file.opts.sourceFileName;
-			var absSourcePath = /^\.?\.\//.test(sourcePath) ? currentFile.replace(/[^/]*$/, "") + sourcePath :
-				sourcePath;
-			const modulePath = 'es6!' + absSourcePath;
-
-			nodePath.replaceWith(state.types.stringLiteral(modulePath));
-			nodePath.node.pathResolved = true;
-		}
-	}
-
-	babel.registerPlugin('apply-es6-recursively', function moduleResolver(args) {
+	babel.registerPlugin("apply-es6-recursively", function (args) {
 		return {
-			name: 'apply-es6-recursively',
+			name: "apply-es6-recursively",
 
 			pre: function () {
 				this.types = args.types;
@@ -65,19 +28,19 @@ _module
 				Program: {
 					enter: function (programPath, state) {
 						programPath.traverse({
-							'ImportDeclaration|ExportDeclaration': function (nodePath, state) {
-								var nodePath = nodePath.get('source');
+							"ImportDeclaration|ExportDeclaration": function (nodePath, state) {
+								var nodePath = nodePath.get("source");
 								if (!state.types.isStringLiteral(nodePath)) {
 									return;
 								}
 
 								const sourcePath = nodePath.node.value;
-								if (sourcePath.indexOf('!') < 0) {
-									// If sourcePath is relative (ex: "./foo"), it's relative to currentFile.
+								if (sourcePath.indexOf("!") < 0) {
+									// If sourcePath is relative (ex: "./foo"), it"s relative to currentFile.
 									var currentFile = state.file.opts.sourceFileName;
 									var absSourcePath = /^\.?\.\//.test(sourcePath) ?
 										currentFile.replace(/[^/]*$/, "") + sourcePath : sourcePath;
-									var modulePath = 'es6!' + absSourcePath;
+									var modulePath = "es6!" + absSourcePath;
 
 									nodePath.replaceWith(state.types.stringLiteral(modulePath));
 									nodePath.node.pathResolved = true;
@@ -90,38 +53,23 @@ _module
 		}
 	});
 
-	var excludedOptions = ['extraPlugins', 'resolveModuleSource'];
-	var pluginOptions = _module.config();
-	var fileExtension = pluginOptions.fileExtension || '.js';
-	var defaultOptions = {
-		plugins: (pluginOptions.extraPlugins || []).concat([
-			'transform-modules-amd',
-			'apply-es6-recursively'
-		])
-	};
-	for (var key in pluginOptions) {
-		if (pluginOptions.hasOwnProperty(key) && excludedOptions.indexOf(key) < 0) {
-			defaultOptions[key] = pluginOptions[key];
-		}
-	}
-
-//>>excludeEnd('excludeBabel')
 	return {
-//>>excludeStart('excludeBabel', pragmas.excludeBabel)
-		load: function (name, req, onload, config) {
-			var sourceFileName = /\./.test(name) ? name : name + fileExtension;
+		load: function (name, req, onload) {
+			var sourceFileName = /\./.test(name) ? name : name + ".js";
 			var url = req.toUrl(sourceFileName);
 
-			if (url.indexOf('empty:') === 0) {
+			if (url.indexOf("empty:") === 0) {
 				return onload();
 			}
 
-			var options = {};
-			for (var key in defaultOptions) {
-				options[key] = defaultOptions[key];
-			}
-			options.sourceFileName = sourceFileName;
-			options.sourceMap = config.isBuild ? false : 'inline';
+			var options = {
+				sourceFileName: sourceFileName,
+				sourceMap: "inline",
+				plugins: [
+					"transform-modules-amd",
+					"apply-es6-recursively"
+				]
+			};
 
 			fetchText(url, function (text) {
 				var code;
@@ -131,19 +79,8 @@ _module
 					return onload.error(error);
 				}
 
-				if (config.isBuild) {
-					_buildMap[name] = code;
-				}
-
 				onload.fromText(code);
 			});
-		},
-
-		write: function (pluginName, moduleName, write) {
-			if (moduleName in _buildMap) {
-				write.asModule(pluginName + '!' + moduleName, _buildMap[moduleName]);
-			}
 		}
-//>>excludeEnd('excludeBabel')
 	};
 });
